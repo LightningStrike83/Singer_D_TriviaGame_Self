311beb1407d15bbb6f3b04574225d076f729dc9a
LightningStrike83 <113542893+LightningStrike83@users.noreply.github.com>
2023-08-03 20:06:22 -0400

    Skeleton
    Added the format

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+let questions = [
+    {
+        question : "What title does the Anna in Fire Emblem Engage like to be referred to as?",
+        answer1 : "Ms. Anna",
+        answer2 : "Madam Anna",
+        answer3 : "Lady Anna",
+        answer4 : "Duchess Anna"
+    },
+
+    {
+        question : "What is the name of the song Eleonora and Tsubasa perform together?",
+        answer1 : "Dream Catcher",
+        answer2 : "She Is",
+        answer3 : "Feel",
+        answer4 : "Give Me",
+    },
+
+    {
+        question : "In Path of Radiance, how old is Oscar?",
+        answer1 : "24",
+        answer2 : "27",
+        answer3 : "18",
+        answer4 : "21",
+    },
+
+    {
+        question : "What did Dorothea say Ferdinand was like in their C-support?",
+        answer1 : "An Aegir Hound",
+        answer2 : "A bee",
+        answer3 : "A goose",
+        answer4 : "A half shaven sheep",
+    },
+
+    {
+        question : "What name did Elincia's Uncle go by when he was brainwashed to be one of the Four Riders?",
+        answer1 : "Amiti",
+        answer2 : "Renning",
+        answer3 : "Bertram",
+        answer4 : "Huey",
+    },
+
+    {
+        question : "Which student has the lowest amount of A-supports in Three Houses?",
+        answer1 : "Felix",
+        answer2 : "Leonie",
+        answer3 : "Sylvain",
+        answer4 : "Bernadetta",
+    },
+
+    {
+        question : "Which of these characters does Jenny Yokobori not voice?",
+        answer1 : "Eleonora Yumizuru (Tokyo Mirage Sessions #FE)",
+        answer2 : "Evelyn (Pokemon Masters EX)",
+        answer3 : "Kasumi (Paladins)",
+        answer4 : "Cinnamoroll (Sanrio)",
+    },
+
+    {
+        question : "Which character inspired Vander to dance so he can 'boogie down' with the younger generation?",
+        answer1 : "Seadall",
+        answer2 : "Timerra",
+        answer3 : "Citrinne",
+        answer4 : "Pandreo",
+    },
+
+    {
+        question : "Which of these possessions belongs to Yunaka?",
+        answer1 : "A prototype Sommie plush made by Lady Anna",
+        answer2 : "A piece of land in noble Brodia territory",
+        answer3 : "A star made of pure gold",
+        answer4 : "A dagger that she nicknamed 'Zappy'",
+    },
+
+    {
+        question : "What did Mysti answer in Emblem Con's Jeopardy panel that Chrom ate and was unpeeled?",
+        answer1 : "Orange",
+        answer2 : "Potato",
+        answer3 : "Banana",
+        answer4 : "Matt Mercer",
+    },
+]
